6725b65fde5816bb8337a7184c894c8a97c3b88b
Pawan Garia <pawangaria@MacBookAir.mshome.net>
2025-05-02 00:01:10 +0530

    changes in map.jsx and added corsheader

--- Frontend/bus_automation/src/pages/map.jsx
+++ Frontend/bus_automation/src/pages/map.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 export default function RouteMap() {
   const [routes, setRoutes] = useState([]);
